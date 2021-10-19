534052705d708bc224c4341a1296029a1f3d3986
Gerald Augustinepillai <gaugust2@uwo.ca>
2021-10-18 23:53:08 -0400

    commit#1

--- scripts.js
+++ scripts.js
@@ -1,9 +1,15 @@
 let nextPlayer = 'X'; // takes a value of either 'X' or 'O' according to the game turns
 
 //initialize the game
+var startButton = document.createElement('button');
+startButton.innerText = 'Click this button to start a new game!';
+document.getElementById('game-over-lbl').appendChild(startButton);
+startButton.addEventListener('click', (initialEvent) => (initialEvent.target.hidden = true));
 
 // use the value stored in the nextPlayer variable to indicate who the next player is
-
+let playerIndicator = document.querySelector('b');
+let playerText = 'Next Player: ';
+playerIndicator.innerText = playerText;
 
 //This call will create the buttons needed for the gameboard.
 createGameBoard()
@@ -11,7 +17,11 @@ createGameBoard()
 function createGameBoard()
 {
     // Programatically add a button with square brackets enclosing an empty space to each cell in the gameboard
-   
+    for(let i = 0; i < 9; i++){
+        let cellID = 'c' + (i+1);
+        var newButton = document.getElement('button');
+        document.getElementById(cellID).appendChild(newButton);
+    }
 }
 
 // Programatically add 'takeCell' as an event listener to all the buttons on the board
@@ -31,9 +41,10 @@ function takeCell(event)
     // Make sure the button is clickable only once (I didn't mention how to do that, look it up :) )
 
     // Check if the game is over
-    if (isGameOver())
+    if (isGameOver() === true)
     {
         // let the lable with the id 'game-over-lbl' display the words 'Game Over' inside <h1> element
+        document.getElementById("h1").innerHTML = "Game Over";
     }
 
     // I'll leave declaring the winner for your intrinsic motivation, it's not required for this assignment 
@@ -42,5 +53,7 @@ function takeCell(event)
 function isGameOver()
 {
     // This function returns true if all the buttons are disabled and false otherwise 
-   
+   let lbl = document.getElementById('game-over-lbl');
+   newHeader.innerText = 'Game Over';
+   lbl.appendChild(newHeader);
 }
